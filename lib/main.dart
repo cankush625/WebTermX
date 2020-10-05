ee54620abea678ab95d85e4d2222dfd31a5a59bc
cankush625 <cankush625@gmail.com>
2020-10-05 22:19:04 +0530

    Terminal made more generic
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
-import 'package:mobile_terminal/terminal/terminal.dart';
-
 import 'package:firebase_core/firebase_core.dart';
+
+import 'package:mobile_terminal/terminal/terminal.dart';
+import 'package:mobile_terminal/terminal/terminal_history.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,6 +15,13 @@ class MobileTerminal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     FlutterStatusbarcolor.setStatusBarColor(Color(0xFF0f088a));
-    return TerminalOutput();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => TerminalOutput(),
+        'history': (context) => History(),
+      },
+    );
   }
 }
--- lib/terminal/terminal.dart
+++ lib/terminal/terminal.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -8,10 +7,9 @@ class TerminalOutput extends StatefulWidget {
   @override
   _TerminalOutputState createState() => _TerminalOutputState();
 }
-// ignore: non_constant_identifier_names
+
 class _TerminalOutputState extends State<TerminalOutput> {
   var fsconnect = FirebaseFirestore.instance;
-  static var screensize;
 
   static TextEditingController _textController = new TextEditingController();
   static Map commandOutput = new Map();
@@ -21,128 +19,136 @@ class _TerminalOutputState extends State<TerminalOutput> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          backgroundColor: Colors.black26,
-          title: Text(
-            'WebTermX',
-          ),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black26,
+        title: Text(
+          'WebTermX',
         ),
-        body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              ListView.builder(
-                itemCount: commandOutput.length,
-                shrinkWrap: true,
-                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                itemBuilder: (BuildContext context, int index) {
-                  screensize = MediaQuery.of(context).size.width;
-                  try {
-                    String key = commandOutput.keys.elementAt(index);
-                    return Column(
-                      children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                            Text(
-                              'mac@ankush',
-                              style: TextStyle(
-                                fontSize: 17,
-                                color: Colors.green,
-                              ),
-                            ),
-                            Text(
-                              '>>>',
-                              style: TextStyle(
-                                fontSize: 17,
-                                color: Colors.red,
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 5),
-                              child: Text(
-                                key,
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Text(
-                          commandOutput[key],
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    );
-                  } catch (e) {
-                    e.toString();
-                    return Text(' ');
-                  }
-                },
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: GestureDetector(
+              child: Icon(
+                Icons.history,
               ),
-              Row(
-                children: <Widget>[
-                  Text(
-                    'mac@ankush',
-                    style: TextStyle(
-                      fontSize: 17,
-                      color: Colors.green,
-                    ),
-                  ),
-                  Text(
-                    '>>>',
-                    style: TextStyle(
-                      fontSize: 17,
-                      color: Colors.red,
-                    ),
-                  ),
-                  Container(
-                    width: 200 * 0.65,
-                    child: TextFormField(
-                      cursorColor: Colors.black,
-                      controller: _textController,
-                      autocorrect: false,
-                      scrollPadding: EdgeInsets.all(0),
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                      onFieldSubmitted: (_textController) async {
-                        commandName = await _textController.toString();
-                        var url = "http://13.235.2.154/cgi-bin/$commandName.py";
-                        var result = await http.get(url);
-                        var data = result.body;
-                        String timestamp = DateTime.now().toString();
-                        fsconnect.collection("terminal_outputs").add({
-                          'commandName': commandName,
-                          'commandOutput': data,
-                          'timestamp': timestamp,
-                        });
-                        setState(() {
-                          output = data;
-                        });
-                        print(commandName);
-                        commandOutput[commandName] = output;
-                      },
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                          enabledBorder: InputBorder.none,
-                          errorBorder: InputBorder.none,
-                          disabledBorder: InputBorder.none,
-                          contentPadding: EdgeInsets.only(left: 5)
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
+              onTap: () {
+                Navigator.pushNamed(context, 'history');
+              },
+            ),
           ),
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            ListView.builder(
+              itemCount: commandOutput.length,
+              shrinkWrap: true,
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+              itemBuilder: (BuildContext context, int index) {
+                try {
+                  String key = commandOutput.keys.elementAt(index);
+                  return Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Text(
+                            'mac@ankush',
+                            style: TextStyle(
+                              fontSize: 17,
+                              color: Colors.green,
+                            ),
+                          ),
+                          Text(
+                            '>>>',
+                            style: TextStyle(
+                              fontSize: 17,
+                              color: Colors.red,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 5),
+                            child: Text(
+                              key,
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        commandOutput[key],
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  );
+                } catch (e) {
+                  e.toString();
+                  return Text(' ');
+                }
+              },
+            ),
+            Row(
+              children: <Widget>[
+                Text(
+                  'mac@ankush',
+                  style: TextStyle(
+                    fontSize: 17,
+                    color: Colors.green,
+                  ),
+                ),
+                Text(
+                  '>>>',
+                  style: TextStyle(
+                    fontSize: 17,
+                    color: Colors.red,
+                  ),
+                ),
+                Container(
+                  width: 200 * 0.65,
+                  child: TextFormField(
+                    cursorColor: Colors.black,
+                    controller: _textController,
+                    autocorrect: false,
+                    scrollPadding: EdgeInsets.all(0),
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    onFieldSubmitted: (_textController) async {
+                      commandName = _textController.toString();
+                      var url = "http://13.127.156.205/cgi-bin/bash.py?commandName=$commandName";
+                      var result = await http.get(url);
+                      var data = result.body;
+                      String timestamp = DateTime.now().toString();
+                      fsconnect.collection("terminal_outputs").add({
+                        'commandName': commandName,
+                        'commandOutput': data,
+                        'timestamp': timestamp,
+                      });
+                      setState(() {
+                        output = data;
+                      });
+                      print(commandName);
+                      commandOutput[commandName] = output;
+                    },
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none,
+                        contentPadding: EdgeInsets.only(left: 5)
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
