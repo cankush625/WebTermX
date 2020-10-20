ad6e73ff2196ff38da8b2c5d731accbb02cb7c3a
cankush625 <cankush625@gmail.com>
2020-10-20 12:48:06 +0530

    terminal UI  changed
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- lib/terminal/terminal.dart
+++ lib/terminal/terminal.dart
@@ -41,114 +41,117 @@ class _TerminalOutputState extends State<TerminalOutput> {
         ],
       ),
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            ListView.builder(
-              itemCount: commandOutput.length,
-              shrinkWrap: true,
-              padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-              itemBuilder: (BuildContext context, int index) {
-                try {
-                  String key = commandOutput.keys.elementAt(index);
-                  return Column(
-                    children: <Widget>[
-                      Row(
-                        children: <Widget>[
-                          Text(
-                            'mac@ankush',
-                            style: TextStyle(
-                              fontSize: 17,
-                              color: Colors.green,
-                            ),
-                          ),
-                          Text(
-                            '>>>',
-                            style: TextStyle(
-                              fontSize: 17,
-                              color: Colors.red,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 5),
-                            child: Text(
-                              key,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 5),
+          child: Column(
+            children: <Widget>[
+              ListView.builder(
+                itemCount: commandOutput.length,
+                shrinkWrap: true,
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                itemBuilder: (BuildContext context, int index) {
+                  try {
+                    String key = commandOutput.keys.elementAt(index);
+                    return Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Text(
+                              'mac:~ ankush',
                               style: TextStyle(
-                                color: Colors.white,
+                                fontSize: 17,
+                                color: Colors.green,
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                      Text(
-                        commandOutput[key],
-                        style: TextStyle(
-                          color: Colors.white,
+                            Text(
+                              '\$',
+                              style: TextStyle(
+                                fontSize: 17,
+                                color: Colors.red,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 5),
+                              child: Text(
+                                key,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
-                  );
-                } catch (e) {
-                  e.toString();
-                  return Text(' ');
-                }
-              },
-            ),
-            Row(
-              children: <Widget>[
-                Text(
-                  'mac@ankush',
-                  style: TextStyle(
-                    fontSize: 17,
-                    color: Colors.green,
-                  ),
-                ),
-                Text(
-                  '>>>',
-                  style: TextStyle(
-                    fontSize: 17,
-                    color: Colors.red,
-                  ),
-                ),
-                Container(
-                  width: 200 * 0.65,
-                  child: TextFormField(
-                    cursorColor: Colors.black,
-                    controller: _textController,
-                    autocorrect: false,
-                    scrollPadding: EdgeInsets.all(0),
+                        Text(
+                          commandOutput[key],
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    );
+                  } catch (e) {
+                    e.toString();
+                    return Text(' ');
+                  }
+                },
+              ),
+              Row(
+                children: <Widget>[
+                  Text(
+                    'mac:~ ankush',
                     style: TextStyle(
-                      color: Colors.white,
-                    ),
-                    onFieldSubmitted: (_textController) async {
-                      commandName = _textController.toString();
-                      var url = "http://13.127.156.205/cgi-bin/bash.py?commandName=$commandName";
-                      var result = await http.get(url);
-                      var data = result.body;
-                      String timestamp = DateTime.now().toString();
-                      fsconnect.collection("terminal_outputs").add({
-                        'commandName': commandName,
-                        'commandOutput': data,
-                        'timestamp': timestamp,
-                      });
-                      setState(() {
-                        output = data;
-                      });
-                      print(commandName);
-                      commandOutput[commandName] = output;
-                    },
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        enabledBorder: InputBorder.none,
-                        errorBorder: InputBorder.none,
-                        disabledBorder: InputBorder.none,
-                        contentPadding: EdgeInsets.only(left: 5)
+                      fontSize: 17,
+                      color: Colors.green,
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  Text(
+                    '\$',
+                    style: TextStyle(
+                      fontSize: 17,
+                      color: Colors.red,
+                    ),
+                  ),
+                  Container(
+                    width: 200 * 0.65,
+                    child: TextFormField(
+                      cursorColor: Colors.black,
+                      controller: _textController,
+                      autocorrect: false,
+                      scrollPadding: EdgeInsets.all(0),
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                      onFieldSubmitted: (_textController) async {
+                        commandName = _textController.toString();
+                        var url = "http://13.127.156.205/cgi-bin/bash.py?commandName=$commandName";
+                        var result = await http.get(url);
+                        var data = result.body;
+                        String timestamp = DateTime.now().toString();
+                        fsconnect.collection("terminal_outputs").add({
+                          'commandName': commandName,
+                          'commandOutput': data,
+                          'timestamp': timestamp,
+                        });
+                        setState(() {
+                          output = data;
+                        });
+                        print(commandName);
+                        commandOutput[commandName] = output;
+                      },
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                          contentPadding: EdgeInsets.only(left: 5)
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
